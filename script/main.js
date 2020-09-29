e02c81226738c3bac47857f5f53f8b0518b53908
vasiliyryzhkovskiy <september86@mail.ru>
2020-09-30 01:07:38 +0300

    add openSubMenu

--- script/main.js
+++ script/main.js
@@ -4,8 +4,9 @@ const btnBurger = document.querySelector(".btn-burger");
 const catalog = document.querySelector(".catalog");
 const overlay = document.querySelector(".overlay");
 const btnClose = document.querySelector(".btn-close");
-const catalogList = document.querySelector('.catalog-list');
-const subCatalog = document.querySelector('.subcatalog');
+const catalogList = document.querySelector(".catalog-list");
+const subCatalog = document.querySelector(".subcatalog");
+const subCatalogHeader = document.querySelector(".subcatalog-header");
 
 const openMenu = () => {
   catalog.classList.add("open");
@@ -17,14 +18,19 @@ const closeMenu = () => {
   overlay.classList.remove("active");
 };
 
-const openSubMenu = event => {
-
-}
+const openSubMenu = (event) => {
+  event.preventDefault();
+  const target = event.target;
+  const itemList = target.closest(".catalog-list__item");
+  if (itemList) {
+    subCatalog.classList.add("subopen");
+  }
+};
 
 btnBurger.addEventListener("click", openMenu);
 btnClose.addEventListener("click", closeMenu);
 overlay.addEventListener("click", closeMenu);
-catalogList.addEventListener("click", openSubMenu)
+catalogList.addEventListener("click", openSubMenu);
 
 document.addEventListener("keydown", (event) => {
   if (event.code === "Escape") {
