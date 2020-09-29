c38a704a39412989670e6d725468e891d05aeacd
vasiliyryzhkovskiy <september86@mail.ru>
2020-09-30 01:27:36 +0300

    day 1 - closeSubMenu ready

--- script/main.js
+++ script/main.js
@@ -7,6 +7,7 @@ const btnClose = document.querySelector(".btn-close");
 const catalogList = document.querySelector(".catalog-list");
 const subCatalog = document.querySelector(".subcatalog");
 const subCatalogHeader = document.querySelector(".subcatalog-header");
+const btnReturn = document.querySelector(".btn-return");
 
 const openMenu = () => {
   catalog.classList.add("open");
@@ -16,6 +17,7 @@ const openMenu = () => {
 const closeMenu = () => {
   catalog.classList.remove("open");
   overlay.classList.remove("active");
+  closeSubMenu();
 };
 
 const openSubMenu = (event) => {
@@ -27,10 +29,15 @@ const openSubMenu = (event) => {
   }
 };
 
+const closeSubMenu = () => {
+  subCatalog.classList.remove("subopen");
+};
+
 btnBurger.addEventListener("click", openMenu);
 btnClose.addEventListener("click", closeMenu);
 overlay.addEventListener("click", closeMenu);
 catalogList.addEventListener("click", openSubMenu);
+btnReturn.addEventListener("click", closeSubMenu);
 
 document.addEventListener("keydown", (event) => {
   if (event.code === "Escape") {
